4b668d464ccf35328d8cfdfcba4adfefe16e6565
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-06-10 18:38:44 +0600

    when seat is zero see red alert

--- src/Shared/SingleClass/SingleClass.jsx
+++ src/Shared/SingleClass/SingleClass.jsx
@@ -10,13 +10,13 @@ const SingleClass = ({ cla }) => {
     const [, refetch] = useCart();
     const navigate = useNavigate();
     const location = useLocation();
-   
+
     const { picture, title, instructorName, availableSeat, students, _id, Price, status } = cla;
     //const [seats, setSeats] = useState(availableSeat);
     const handleAddToCart = item => {
         console.log('item', item)
-      
-  
+
+
         if (user && user.email) {
             const cartItem = { classId: _id, title, instructorName, availableSeat, students, picture, Price, email: user.email }
             fetch('http://localhost:5000/carts', {
@@ -57,40 +57,80 @@ const SingleClass = ({ cla }) => {
         }
     }
     return (
-        <>{ status ==='approved' &&
-            <div className="card w-96 bg-base-100 shadow-xl cursor-pointer group ">
-                <figure className="px-10 pt-10">
-                    <img src={picture} alt="Shoes" className="rounded-xl h-[250px] w-[390px] group-hover:scale-150 transition" />
-                </figure>
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">{title}</h2>
-                    <p className="font-bold">Instructor Name: {instructorName}</p>
-                    <div className="flex gap-8">
-                        <p>Seats Available: {availableSeat}</p>
-                        <p>Total Students:{students}</p>
-                    </div>
-                    {
-                        role === 'admin' ? (<>
+        <>{status === 'approved' &&
+            <div>
 
-                            <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]" disabled>Selected The Course</button>
-                        </>)
-                            :
-                            role === 'instructor' ? (<>
+                {availableSeat === 0 ?
+                    <div className="card w-96 bg-red-500 shadow-xl cursor-pointer group ">
+                        <figure className="px-10 pt-10">
+                            <img src={picture} alt="Shoes" className="rounded-xl h-[250px] w-[390px] group-hover:scale-150 transition" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">{title}</h2>
+                            <p className="font-bold">Instructor Name: {instructorName}</p>
+                            <div className="flex gap-8">
+                                <p>Seats Available: {availableSeat}</p>
+                                <p>Total Students:{students}</p>
+                            </div>
+                            {
+                                role === 'admin' ? (<>
 
-                                <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]" disabled>Selected The Course</button>
-
-
-
-                            </>)
-                                :
-                                (<>
-                                    <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]">Selected The Course</button>
-
+                                    <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]" disabled>Selected The Course</button>
                                 </>)
-                    }
+                                    :
+                                    role === 'instructor' ? (<>
+
+                                        <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]" disabled>Selected The Course</button>
 
 
-                </div>
+
+                                    </>)
+                                        :
+                                        (<>
+                                            <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]"disabled>Selected The Course</button>
+
+                                        </>)
+                            }
+
+
+                        </div>
+                    </div>
+                    :
+                    <div className="card w-96 bg-base-100 shadow-xl cursor-pointer group ">
+                        <figure className="px-10 pt-10">
+                            <img src={picture} alt="Shoes" className="rounded-xl h-[250px] w-[390px] group-hover:scale-150 transition" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">{title}</h2>
+                            <p className="font-bold">Instructor Name: {instructorName}</p>
+                            <div className="flex gap-8">
+                                <p>Seats Available: {availableSeat}</p>
+                                <p>Total Students:{students}</p>
+                            </div>
+                            {
+                                role === 'admin' ? (<>
+
+                                    <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]" disabled>Selected The Course</button>
+                                </>)
+                                    :
+                                    role === 'instructor' ? (<>
+
+                                        <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]" disabled>Selected The Course</button>
+
+
+
+                                    </>)
+                                        :
+                                        (<>
+                                            <button onClick={() => handleAddToCart(cla)} className="rounded-full btn btn-outline bg-slate-200 text-[#BB8506] border-0 border-b-4 mt-4 hover:text-[#BB8506]">Selected The Course</button>
+
+                                        </>)
+                            }
+
+
+                        </div>
+                    </div>
+                }
             </div>
         }</>
     );
